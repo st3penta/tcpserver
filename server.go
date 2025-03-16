2165ad3476e6041edaf721b259d745f339c4f082
st3penta <71923697+st3penta@users.noreply.github.com>
2025-03-16 11:45:22 +0100

    style: better server messages

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"tcpserver/commands"
@@ -28,32 +29,36 @@ func (s *Server) Start() {
 	}
 	defer ln.Close()
 
-	fmt.Println("Starting server...")
+	fmt.Println("Server ready for incoming connections...")
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		// Accept incoming connections (in async, so that we can accept multiple connections)
+		// Accept incoming connections (in async, so that we can accept many)
 		go s.handleConnection(conn)
 	}
 }
 
 func (s *Server) handleConnection(conn net.Conn) {
 
-	fmt.Println("Waiting for new messages...")
+	fmt.Println("Connection established, waiting for commands...")
 	for {
 
 		cmd, cmdErr := commands.ParseCommand(conn)
+		if cmdErr == io.EOF {
+			fmt.Println("Client disconnected")
+			break
+		}
 		if cmdErr != nil {
-			fmt.Println("Error while parsing command: ", cmdErr)
+			fmt.Println("Error while parsing command:", cmdErr)
 			break
 		}
 
 		resp, procErr := cmd.Process()
 		if procErr != nil {
-			fmt.Println("Error while processing command: ", procErr)
+			fmt.Println("Error while processing command:", procErr)
 			break
 		}
 
